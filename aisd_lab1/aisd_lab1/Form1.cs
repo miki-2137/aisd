205b10646f2f494078397ea202b3ed197fbf600a
miki-2137 <92546137+miki-2137@users.noreply.github.com>
2024-10-02 09:30:59 +0200

    lab1 upload

---
+++ aisd_lab1/aisd_lab1/Form1.cs
@@ -0,0 +1,15 @@
+namespace aisd_lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("AiSD jest najlepsze!!!");
+        }
+    }
+}
