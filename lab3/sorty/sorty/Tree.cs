e43652d239594dd624ee30969f7400ed7133fb07
miki-2137 <92546137+miki-2137@users.noreply.github.com>
2024-10-23 09:29:00 +0200

    lab4 update

---
+++ lab3/sorty/sorty/Tree.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sorty
+{
+    internal class Tree
+    {
+        public NodeT korzen;
+
+        //napisac metode Add do bst(binary search tree) i InOrder(???)
+    }
+}
