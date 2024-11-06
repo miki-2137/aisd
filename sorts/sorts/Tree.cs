2deb6cb88cf935c9d35b82fa656bd69f7ab22eab
miki-2137 <92546137+miki-2137@users.noreply.github.com>
2024-11-06 09:09:41 +0100

    Update Tree.cs

--- sorts/sorts/Tree.cs
+++ sorts/sorts/Tree.cs
@@ -5,13 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
-namespace sorts
+namespace sorty
 {
     internal class Tree
     {
         public NodeT korzen;
 
-        public void AddInto(NodeT node)
+        public void Add(NodeT node)
         {
             if (this.korzen == null)
             {
@@ -19,17 +19,17 @@ namespace sorts
             }
             else
             {
-                Add(node, this.korzen);
+                AddInto(node, this.korzen);
             }
         }
 
-        public void Add(NodeT node, NodeT korzen)
+        public void AddInto(NodeT node, NodeT korzen)
         {
             if (node.data < korzen.data)
             {
                 if (korzen.lewe != null)
                 {
-                    Add(node, korzen.lewe);
+                    AddInto(node, korzen.lewe);
                 }
                 else
                 {
@@ -40,7 +40,7 @@ namespace sorts
             {
                 if (korzen.prawe != null)
                 {
-                    Add(node, korzen.prawe);
+                    AddInto(node, korzen.prawe);
                 }
                 else
                 {
@@ -49,27 +49,26 @@ namespace sorts
             }
         }
 
-        public void InOrder(NodeT root)
+        public void InOrder(NodeT korzen)
         {
-            if (root.lewe != null)
+            if (korzen.lewe != null)
             {
-                InOrder(root.lewe);
+                InOrder(korzen.lewe);
             }
-            Console.WriteLine(root.data);
-            if (root.prawe != null)
+            Console.WriteLine(korzen.data);
+            if (korzen.prawe != null)
             {
-                InOrder(root.prawe);
+                InOrder(korzen.prawe);
             }
         }
-
         public static void Main(string[] args)
         {
             Tree tr = new Tree();
-            tr.AddInto(new NodeT(5));
-            tr.AddInto(new NodeT(3));
-            tr.AddInto(new NodeT(7));
-            tr.AddInto(new NodeT(1));
-            tr.AddInto(new NodeT(9));
+            tr.Add(new NodeT(5));
+            tr.Add(new NodeT(3));
+            tr.Add(new NodeT(7));
+            tr.Add(new NodeT(1));
+            tr.Add(new NodeT(9));
 
             /*Console.WriteLine(tr.korzen.data);
             Console.WriteLine(tr.korzen.lewe.data);
