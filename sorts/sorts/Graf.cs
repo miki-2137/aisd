632d0f2fbb795cb4be776eca85510bcffb5f02d8
miki-2137 <92546137+miki-2137@users.noreply.github.com>
2024-11-20 09:23:09 +0100

    Add files via upload

---
+++ sorts/sorts/Graf.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sorty
+{
+    internal class Graf
+    {
+        public List<NodeG> nodes = new List<NodeG>();
+    }
+}
---
+++ sorts/sorts/NodeG.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sorty
+{
+    internal class NodeG
+    {
+        public List<NodeG> sasiedzi = new List<NodeG>();
+        public int data;
+
+        public NodeG(int data)
+        {
+            this.data = data;
+        }
+
+        public override string ToString()
+        {
+            return this.data.ToString();
+        }
+
+        public void DodajSasiada(NodeG sasiad)
+        {
+            if (!this.sasiedzi.Contains(sasiad))
+            {
+                this.sasiedzi.Add(sasiad);
+            }
+        }
+        /*napisac przechodzenie wszerz(dwie petle, jedna po nowej liscie, druga po sosiadach danego wezla;wierzcolek jako parametr) 
+         i wglab(rekurencja;wierzcholek i lista jako parametry)*/
+        public void Move(NodeG a)
+        {
+            List<NodeG> odwiedzone = new List<NodeG>();
+            for(int i = 0; i < this.sasiedzi.Count; i++)
+            {
+
+            }
+        }
+    }
+}
